3e16a7c74dd1eeb195dc30643a433d56e6111ddb
Light-Creator <ibadulasha@ya.ru>
2025-04-03 05:22:27 +0300

    new logic with json settings

--- parse_mail/main.py
+++ parse_mail/main.py
@@ -2,67 +2,66 @@ import requests
 import json
 import argparse
 
+from urllib.parse import unquote, quote_plus
+
 
 class Mailer:
-    def __init__(self, session_file):
+    def __init__(self, settings_file: str):
         self.session = requests.Session()
-        
-        self.session_file = session_file 
+        self.settings_file = settings_file
+        self.settings = None
+        self.get_mail_by_id_postdata = None
 
-        self.session_cookies = None
-        self.mails_headers = None
         self.mails = None
 
-        self.get_session_cookies()
-        self.get_mail_headers()
+        self.load_settings()
+        self.load_headers()
+
+    def load_settings(self):
+        with open(self.settings_file, "r") as f:
+            self.settings = json.load(f, strict=False)
+
+    def load_headers(self):
+        postdata = unquote(self.settings["headers"]["get_conversation_by_id"]["X-Owa-Urlpostdata"]) 
+        self.get_mail_by_id_postdata = json.loads(postdata)
         
-    def get_session_cookies(self) -> None:
-        try:
-            f = open(self.session_file, "r")
-            cookies = f.read().strip()
-            headers = {
-                    "Cookie": cookies
-                    }
-
-            f.close()
-            self.session_cookies = {"Cookie": cookies}
-        except FileNotFoundError:
-           raise FileNotFoundError(f"file {self.session_file} not found")
-    
-    def get_mail_headers(self) -> None:
-        self.mails_headers = {"X-Owa-Urlpostdata": "%7B%22__type%22%3A%22FindConversationJsonRequest%3A%23Exchange%22%2C%22Header%22%3A%7B%22__type%22%3A%22JsonRequestHeaders%3A%23Exchange%22%2C%22RequestServerVersion%22%3A%22V2016_02_03%22%2C%22TimeZoneContext%22%3A%7B%22__type%22%3A%22TimeZoneContext%3A%23Exchange%22%2C%22TimeZoneDefinition%22%3A%7B%22__type%22%3A%22TimeZoneDefinitionType%3A%23Exchange%22%2C%22Id%22%3A%22Russian%20Standard%20Time%22%7D%7D%7D%2C%22Body%22%3A%7B%22__type%22%3A%22FindConversationRequest%3A%23Exchange%22%2C%22ParentFolderId%22%3A%7B%22__type%22%3A%22TargetFolderId%3A%23Exchange%22%2C%22BaseFolderId%22%3A%7B%22__type%22%3A%22DistinguishedFolderId%3A%23Exchange%22%2C%22Id%22%3A%22inbox%22%7D%7D%2C%22ConversationShape%22%3A%7B%22__type%22%3A%22ConversationResponseShape%3A%23Exchange%22%2C%22BaseShape%22%3A%22IdOnly%22%7D%2C%22ShapeName%22%3A%22ConversationListView%22%2C%22Paging%22%3A%7B%22__type%22%3A%22IndexedPageView%3A%23Exchange%22%2C%22BasePoint%22%3A%22Beginning%22%2C%22Offset%22%3A0%2C%22MaxEntriesReturned%22%3A500%7D%2C%22ViewFilter%22%3A%22All%22%2C%22FocusedViewFilter%22%3A-1%2C%22SortOrder%22%3A%5B%7B%22__type%22%3A%22SortResults%3A%23Exchange%22%2C%22Order%22%3A%22Descending%22%2C%22Path%22%3A%7B%22__type%22%3A%22PropertyUri%3A%23Exchange%22%2C%22FieldURI%22%3A%22ConversationLastDeliveryOrRenewTime%22%7D%7D%2C%7B%22__type%22%3A%22SortResults%3A%23Exchange%22%2C%22Order%22%3A%22Descending%22%2C%22Path%22%3A%7B%22__type%22%3A%22PropertyUri%3A%23Exchange%22%2C%22FieldURI%22%3A%22ConversationLastDeliveryTime%22%7D%7D%5D%7D%7D", "Action": "FindConversation"}
-
-    def set_headers_for_conversations_req(self) -> None:
+    def get_all_mails(self):
         self.session.headers.clear()
-        self.session.headers.update(self.session_cookies)
-        self.session.headers.update(self.mail_headers)
+        self.session.headers.update(self.settings["session"])
+        self.session.headers.update(self.settings["headers"]["get_conversations"])
 
-    def get_all_mails(self) -> None:
-        self.set_headers_for_conversations_req()
-        res = self.session.get("https://mail.spbstu.ru/owa/service.svc?action=FindConversation")
+        res = self.session.get(self.settings["urls"]["get_conversations"])
         self.mails = json.loads(res.text)
+    
+    def get_mail_by_id(self, id: str):
+        self.get_mail_by_id_postdata["Body"]["Conversations"]["ConversationId"]["Id"] = id
+        
+        self.session.headers.clear()
+        self.session.headers.update(self.settings["session"])
+        self.session.headers.update({"X-Owa-Urlpostdata": quote_plus(self.get_mail_by_id_postdata), "Action": "GetConversationItems"})
 
-    def filter_mails_by(self, filter_name: str, filter_value: str) -> None:
-        pass
+        res = self.session.get(self.settings["urls"]["get_conversation_by_id"])
 
-    def send_mail(self, topic: str, body: str) -> None:
-        pass
-
+    def get_full_mails_by_author(self, autor: str):
+        for mail in self.mails:
+            if author in mail["UniqueRecipients"]:
+                get_mail_by_id(mail["ConversationId"]["Id"])
+        
 
 def main() -> None:
     parser = argparse.ArgumentParser()
 
     parser.add_argument("operation", type=str, help="Operation [send or get]")
-    parser.add_argument("session_file", type=str, help="File with session creds")
+    parser.add_argument("settings_file", type=str, help="File with settings (json format)")
     
     parser.add_argument("-t", "--topic", type=str, help="Filter for mail by topic")
     parser.add_argument("-d", "--date", type=str, help="Filter for mail by date")
     parser.add_argument("-a", "--autor", type=str, help="Filter for mail by author")
     parser.add_argument("-b", "--body", type=str, help="Filter for mail by body text")
     
-    
+    args = parser.parse_args()
 
-    mailer = Mailer("session.txt")
+    mailer = Mailer(args.settings_file)
 
     
     
