46b02a6cb081238fe205c83bd67d84544cc62fc3
Light-creator <ibadulasha@ya.ru>
2025-04-03 07:06:00 +0300

    add settings.json

--- parse_mail/main.py
+++ parse_mail/main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import parseargs
+import argparse
 
 
 class Mailer:
@@ -26,16 +26,16 @@ class Mailer:
 
             f.close()
             self.session_cookies = {"Cookie": cookies}
-       except FileNotFoundError:
+        except FileNotFoundError:
            raise FileNotFoundError(f"file {self.session_file} not found")
     
     def get_mail_headers(self) -> None:
         self.mails_headers = {"X-Owa-Urlpostdata": "%7B%22__type%22%3A%22FindConversationJsonRequest%3A%23Exchange%22%2C%22Header%22%3A%7B%22__type%22%3A%22JsonRequestHeaders%3A%23Exchange%22%2C%22RequestServerVersion%22%3A%22V2016_02_03%22%2C%22TimeZoneContext%22%3A%7B%22__type%22%3A%22TimeZoneContext%3A%23Exchange%22%2C%22TimeZoneDefinition%22%3A%7B%22__type%22%3A%22TimeZoneDefinitionType%3A%23Exchange%22%2C%22Id%22%3A%22Russian%20Standard%20Time%22%7D%7D%7D%2C%22Body%22%3A%7B%22__type%22%3A%22FindConversationRequest%3A%23Exchange%22%2C%22ParentFolderId%22%3A%7B%22__type%22%3A%22TargetFolderId%3A%23Exchange%22%2C%22BaseFolderId%22%3A%7B%22__type%22%3A%22DistinguishedFolderId%3A%23Exchange%22%2C%22Id%22%3A%22inbox%22%7D%7D%2C%22ConversationShape%22%3A%7B%22__type%22%3A%22ConversationResponseShape%3A%23Exchange%22%2C%22BaseShape%22%3A%22IdOnly%22%7D%2C%22ShapeName%22%3A%22ConversationListView%22%2C%22Paging%22%3A%7B%22__type%22%3A%22IndexedPageView%3A%23Exchange%22%2C%22BasePoint%22%3A%22Beginning%22%2C%22Offset%22%3A0%2C%22MaxEntriesReturned%22%3A500%7D%2C%22ViewFilter%22%3A%22All%22%2C%22FocusedViewFilter%22%3A-1%2C%22SortOrder%22%3A%5B%7B%22__type%22%3A%22SortResults%3A%23Exchange%22%2C%22Order%22%3A%22Descending%22%2C%22Path%22%3A%7B%22__type%22%3A%22PropertyUri%3A%23Exchange%22%2C%22FieldURI%22%3A%22ConversationLastDeliveryOrRenewTime%22%7D%7D%2C%7B%22__type%22%3A%22SortResults%3A%23Exchange%22%2C%22Order%22%3A%22Descending%22%2C%22Path%22%3A%7B%22__type%22%3A%22PropertyUri%3A%23Exchange%22%2C%22FieldURI%22%3A%22ConversationLastDeliveryTime%22%7D%7D%5D%7D%7D", "Action": "FindConversation"}
 
     def set_headers_for_conversations_req(self) -> None:
-        s.headers.clear()
-        s.headers.update(self.session_cookies)
-        s.headers.update(self.mail_headers)
+        self.session.headers.clear()
+        self.session.headers.update(self.session_cookies)
+        self.session.headers.update(self.mail_headers)
 
     def get_all_mails(self) -> None:
         self.set_headers_for_conversations_req()
